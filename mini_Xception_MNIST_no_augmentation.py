61cd4b04ac80a2b54f3716c72d182d3dace8386d
Jeonghyun Kwak <chawoni21@naver.com>
2025-01-27 17:11:03 +0900

    eleventh commit

---
+++ mini_Xception_MNIST_no_augmentation.py
@@ -0,0 +1,153 @@
+from tensorflow.keras.datasets import mnist
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+train_images = train_images.reshape((60000, 28, 28, 1))
+train_images = train_images.astype("float32") / 255
+test_images = test_images.reshape((10000, 28, 28, 1))
+test_images = test_images.astype("float32") / 255
+
+print(len(train_images[50000:]))
+print(len(train_labels[50000:]))
+
+valid_images = train_images[50000:]
+valid_labels = train_labels[50000:]
+
+train_images = train_images[:50000]
+train_labels = train_labels[:50000]
+
+test_images = test_images
+test_labels = test_labels
+
+print(len(train_images))
+print(len(valid_images))
+print(len(test_images))
+
+import matplotlib.pyplot as plt
+
+def preview_dataset(image):
+    # plot the sample
+    fig = plt.figure
+    plt.imshow(image, cmap='gray')
+    plt.show()
+
+preview_dataset(train_images[10])
+
+import tensorflow as tf
+
+train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+valid_dataset = tf.data.Dataset.from_tensor_slices((valid_images, valid_labels))
+test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
+
+print(train_dataset)
+print(valid_dataset)
+print(test_dataset)
+
+train_ds = train_dataset.shuffle(1000).batch(128).prefetch(tf.data.experimental.AUTOTUNE)
+valid_ds = valid_dataset.shuffle(1000).batch(128).prefetch(tf.data.experimental.AUTOTUNE)
+test_ds = test_dataset.shuffle(1000).batch(128).prefetch(tf.data.experimental.AUTOTUNE)
+
+print(train_ds)
+print(valid_ds)
+print(test_ds)
+
+# learning rate = 1e-4
+from tensorflow import keras
+from tensorflow.keras import layers
+
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.2),
+    ]
+)
+
+inputs = keras.Input(shape=(28, 28, 1))
+#x = data_augmentation(inputs)
+
+x = layers.Rescaling(1./255)(inputs)
+x = layers.Conv2D(filters=32, kernel_size=5, use_bias=False)(inputs)
+
+for size in [32, 64, 128, 256, 512]:
+    residual = x
+
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    x = layers.SeparableConv2D(size, 3, padding="same", use_bias=False)(x)
+
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    x = layers.SeparableConv2D(size, 3, padding="same", use_bias=False)(x)
+
+    x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+    residual = layers.Conv2D(
+        size, 1, strides=2, padding="same", use_bias=False)(residual)
+    x = layers.add([x, residual])
+
+x = layers.GlobalAveragePooling2D()(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(10, activation="softmax")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=keras.optimizers.Adam(learning_rate=1e-4),
+              metrics=["accuracy"])
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        filepath = 'xception_50epch',
+        save_best_only=True,
+        monitor='val_loss'
+    )
+]
+history = model.fit(
+    train_ds,
+    epochs=50,
+    validation_data=valid_ds,
+    callbacks=callbacks)
+
+import matplotlib.pyplot as plt
+
+accuracy = history.history["accuracy"]
+val_accuracy = history.history["val_accuracy"]
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
+epochs = range(1, len(accuracy) + 1)
+plt.plot(epochs, accuracy, "bo", label="Training accuracy")
+plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
+plt.title("Training and validation accuracy")
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, "bo", label="Training loss")
+plt.plot(epochs, val_loss, "b", label="Validation loss")
+plt.title("Training and validation loss")
+plt.legend()
+plt.show()
+
+import time
+import matplotlib.pyplot as plt
+from tensorflow import keras
+
+test_model = keras.models.load_model('xception_50epch')
+start_time = time.time()
+test_loss, test_acc = test_model.evaluate(test_ds)
+end_time = time.time()
+eval_time = end_time - start_time
+
+print(f"Evaluation Time: {eval_time} seconds")
+print(f"Test Accuracy: {test_acc}")
+print(f"Test Loss: {test_loss}")
+
+#그래프 그리기
+metrics = ['Accuracy', 'Loss', 'Evaluation Time']
+values = [test_acc, test_loss, eval_time]
+
+plt.figure(figsize=(10, 6))
+plt.bar(metrics, values, color=['blue', 'orange', 'green'])
+plt.xlabel('Metrics')
+plt.ylabel('Values')
+plt.title('Model Evaluation Metrics')
+plt.ylim(0, max(values) + 1)  # Adjust
