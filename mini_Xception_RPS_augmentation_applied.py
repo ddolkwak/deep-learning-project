4782f7d093d3d8d04c56e1b5da46a756eb77de1c
Jeonghyun Kwak <chawoni21@naver.com>
2025-01-27 20:56:08 +0900

    eighteenth commit

---
+++ mini_Xception_RPS_augmentation_applied.py
@@ -0,0 +1,413 @@
+# Import necessary libraries
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+# Load the dataset
+dataset, info = tfds.load('rock_paper_scissors', with_info=True, as_supervised=True)
+
+print(dataset)
+print(info)
+
+# Split the dataset
+train = dataset['train']
+test = dataset['test']
+
+# Define the split for training and validation
+split_percentage = 0.9
+train_size = int(split_percentage * info.splits['train'].num_examples)
+val_size = info.splits['train'].num_examples - train_size
+
+# Use the 'train_test_split' function from tf.data.experimental
+train_data = train.take(train_size)
+valid_data = train.skip(train_size)
+test_data = test
+
+# Print dataset sizes
+print(f"Training set size: {train_size}")
+print(f"Validation set size: {val_size}")
+print(f"Test set size: {info.splits['test'].num_examples}")
+
+print(train_data)
+print(valid_data)
+print(test_data)
+
+import matplotlib.pyplot as plt
+
+
+def preview_dataset(dataset):
+    plt.figure(figsize=(12, 12))
+    plot_index = 0
+    for features in dataset.take(4):
+        (image, label) = features
+        plot_index += 1
+        plt.subplot(3, 4, plot_index)
+        # plt.axis('Off')
+        label = info.features['label'].int2str(label.numpy())
+        plt.title('Label: %s' % label)
+        plt.imshow(image.numpy())
+
+#def preprocess(image, label):
+#    image = tf.image.resize(image, (180, 180))
+#    image = image / 255.0  # Normalize to [0,1] range
+#    print(image)
+#    return image, label
+
+train_ds = train_data.shuffle(100).batch(32).prefetch(tf.data.experimental.AUTOTUNE)
+valid_ds = valid_data.shuffle(100).batch(32).prefetch(tf.data.experimental.AUTOTUNE)
+test_ds = test_data.shuffle(100).batch(32).prefetch(tf.data.experimental.AUTOTUNE)
+
+
+print(train_ds)
+print(valid_ds)
+print(test_ds)
+
+for image_batch, label_batch in valid_ds.take(1):
+  pass
+
+print(image_batch.shape)
+print(label_batch)
+
+print(len(train_ds))
+print(len(valid_ds))
+
+# learning rate = 1e-5
+from tensorflow import keras
+from tensorflow.keras import layers
+
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.2),
+    ]
+)
+
+inputs = keras.Input(shape=(300, 300, 3))
+x = data_augmentation(inputs)
+
+x = layers.Rescaling(1./255)(x)
+x = layers.Conv2D(filters=32, kernel_size=5, use_bias=False)(inputs)
+
+for size in [32, 64, 128, 256, 512]:
+    residual = x
+
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    x = layers.SeparableConv2D(size, 3, padding="same", use_bias=False)(x)
+
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    x = layers.SeparableConv2D(size, 3, padding="same", use_bias=False)(x)
+
+    x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+    residual = layers.Conv2D(
+        size, 1, strides=2, padding="same", use_bias=False)(residual)
+    x = layers.add([x, residual])
+
+x = layers.GlobalAveragePooling2D()(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(3, activation="softmax")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=keras.optimizers.Adam(learning_rate=1e-5),
+              metrics=["accuracy"])
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        filepath = 'xception_100epch',
+        save_best_only=True,
+        monitor='val_loss'
+    )
+]
+history = model.fit(
+    train_ds,
+    epochs=100,
+    validation_data=valid_ds,
+    callbacks=callbacks)
+
+import matplotlib.pyplot as plt
+
+accuracy = history.history["accuracy"]
+val_accuracy = history.history["val_accuracy"]
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
+epochs = range(1, len(accuracy) + 1)
+plt.plot(epochs, accuracy, "bo", label="Training accuracy")
+plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
+plt.title("Training and validation accuracy")
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, "bo", label="Training loss")
+plt.plot(epochs, val_loss, "b", label="Validation loss")
+plt.title("Training and validation loss")
+plt.legend()
+plt.show()
+
+test_model = keras.models.load_model('xception_100epch')
+test_loss, test_acc = test_model.evaluate(test_ds)
+print(f"Test Accuracy : {test_acc}, Test Loss : {test_loss}")
+
+# learning rate = 1e-4
+from tensorflow import keras
+from tensorflow.keras import layers
+
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.2),
+    ]
+)
+
+inputs = keras.Input(shape=(300, 300, 3))
+x = data_augmentation(inputs)
+
+x = layers.Rescaling(1./255)(x)
+x = layers.Conv2D(filters=32, kernel_size=5, use_bias=False)(inputs)
+
+for size in [32, 64, 128, 256, 512]:
+    residual = x
+
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    x = layers.SeparableConv2D(size, 3, padding="same", use_bias=False)(x)
+
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    x = layers.SeparableConv2D(size, 3, padding="same", use_bias=False)(x)
+
+    x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+    residual = layers.Conv2D(
+        size, 1, strides=2, padding="same", use_bias=False)(residual)
+    x = layers.add([x, residual])
+
+x = layers.GlobalAveragePooling2D()(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(3, activation="softmax")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=keras.optimizers.Adam(learning_rate=1e-4),
+              metrics=["accuracy"])
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        filepath = 'xception_100epch',
+        save_best_only=True,
+        monitor='val_loss'
+    )
+]
+history = model.fit(
+    train_ds,
+    epochs=100,
+    validation_data=valid_ds,
+    callbacks=callbacks)
+
+import matplotlib.pyplot as plt
+
+accuracy = history.history["accuracy"]
+val_accuracy = history.history["val_accuracy"]
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
+epochs = range(1, len(accuracy) + 1)
+plt.plot(epochs, accuracy, "bo", label="Training accuracy")
+plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
+plt.title("Training and validation accuracy")
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, "bo", label="Training loss")
+plt.plot(epochs, val_loss, "b", label="Validation loss")
+plt.title("Training and validation loss")
+plt.legend()
+plt.show()
+
+import time
+import matplotlib.pyplot as plt
+from tensorflow import keras
+
+test_model = keras.models.load_model('xception_100epch')
+start_time = time.time()
+test_loss, test_acc = test_model.evaluate(test_ds)
+end_time = time.time()
+eval_time = end_time - start_time
+
+print(f"Evaluation Time: {eval_time} seconds")
+print(f"Test Accuracy: {test_acc}")
+print(f"Test Loss: {test_loss}")
+
+#그래프 그리기
+metrics = ['Accuracy', 'Loss', 'Evaluation Time']
+values = [test_acc, test_loss, eval_time]
+
+plt.figure(figsize=(10, 6))
+plt.bar(metrics, values, color=['blue', 'orange', 'green'])
+plt.xlabel('Metrics')
+plt.ylabel('Values')
+plt.title('Model Evaluation Metrics')
+plt.ylim(0, max(values) + 1)  # Adjust
+
+
+# learning rate = 5e-5
+from tensorflow import keras
+from tensorflow.keras import layers
+
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.2),
+    ]
+)
+
+inputs = keras.Input(shape=(300, 300, 3))
+x = data_augmentation(inputs)
+
+x = layers.Rescaling(1./255)(x)
+x = layers.Conv2D(filters=32, kernel_size=5, use_bias=False)(inputs)
+
+for size in [32, 64, 128, 256, 512]:
+    residual = x
+
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    x = layers.SeparableConv2D(size, 3, padding="same", use_bias=False)(x)
+
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    x = layers.SeparableConv2D(size, 3, padding="same", use_bias=False)(x)
+
+    x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+    residual = layers.Conv2D(
+        size, 1, strides=2, padding="same", use_bias=False)(residual)
+    x = layers.add([x, residual])
+
+x = layers.GlobalAveragePooling2D()(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(3, activation="softmax")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=keras.optimizers.Adam(learning_rate=5e-5),
+              metrics=["accuracy"])
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        filepath = 'xception_100epch',
+        save_best_only=True,
+        monitor='val_loss'
+    )
+]
+history = model.fit(
+    train_ds,
+    epochs=100,
+    validation_data=valid_ds,
+    callbacks=callbacks)
+
+import matplotlib.pyplot as plt
+
+accuracy = history.history["accuracy"]
+val_accuracy = history.history["val_accuracy"]
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
+epochs = range(1, len(accuracy) + 1)
+plt.plot(epochs, accuracy, "bo", label="Training accuracy")
+plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
+plt.title("Training and validation accuracy")
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, "bo", label="Training loss")
+plt.plot(epochs, val_loss, "b", label="Validation loss")
+plt.title("Training and validation loss")
+plt.legend()
+plt.show()
+
+test_model = keras.models.load_model('xception_100epch')
+test_loss, test_acc = test_model.evaluate(test_ds)
+print(f"Test Accuracy : {test_acc}, Test Loss : {test_loss}")
+
+# learning rate = 2e-5
+from tensorflow import keras
+from tensorflow.keras import layers
+
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.2),
+    ]
+)
+
+inputs = keras.Input(shape=(300, 300, 3))
+x = data_augmentation(inputs)
+
+x = layers.Rescaling(1./255)(x)
+x = layers.Conv2D(filters=32, kernel_size=5, use_bias=False)(inputs)
+
+for size in [32, 64, 128, 256, 512]:
+    residual = x
+
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    x = layers.SeparableConv2D(size, 3, padding="same", use_bias=False)(x)
+
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    x = layers.SeparableConv2D(size, 3, padding="same", use_bias=False)(x)
+
+    x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+    residual = layers.Conv2D(
+        size, 1, strides=2, padding="same", use_bias=False)(residual)
+    x = layers.add([x, residual])
+
+x = layers.GlobalAveragePooling2D()(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(3, activation="softmax")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=keras.optimizers.Adam(learning_rate=2e-5),
+              metrics=["accuracy"])
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        filepath = 'xception_100epch',
+        save_best_only=True,
+        monitor='val_loss'
+    )
+]
+history = model.fit(
+    train_ds,
+    epochs=100,
+    validation_data=valid_ds,
+    callbacks=callbacks)
+
+import matplotlib.pyplot as plt
+
+accuracy = history.history["accuracy"]
+val_accuracy = history.history["val_accuracy"]
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
+epochs = range(1, len(accuracy) + 1)
+plt.plot(epochs, accuracy, "bo", label="Training accuracy")
+plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
+plt.title("Training and validation accuracy")
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, "bo", label="Training loss")
+plt.plot(epochs, val_loss, "b", label="Validation loss")
+plt.title("Training and validation loss")
+plt.legend()
+plt.show()
+
+test_model = keras.models.load_model('xception_100epch')
+test_loss, test_acc = test_model.evaluate(test_ds)
+print(f"Test Accuracy : {test_acc}, Test Loss : {test_loss}")
